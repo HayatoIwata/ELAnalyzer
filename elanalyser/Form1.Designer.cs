ec8a70835e4f6ace168e89d566fe8ec4f82d02ea
HayatoIwata <iwata.hayato.525@gmail.com>
2015-10-24 16:20:39 +0900

    v1.00正規版

---
+++ elanalyser/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace elanalyser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.OpenRegistDialog = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.MainTab = new System.Windows.Forms.TabControl();
+            this.UserRythm = new System.Windows.Forms.TabPage();
+            this.SaveAllSFF = new System.Windows.Forms.Button();
+            this.SaveSelectedSFF = new System.Windows.Forms.Button();
+            this.UserRythmList = new System.Windows.Forms.ListView();
+            this.UserNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SFFStatus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SFFSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.UserRythmMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.RenameAndSaveSFF = new System.Windows.Forms.ToolStripMenuItem();
+            this.ReadSFF = new System.Windows.Forms.ToolStripMenuItem();
+            this.InitializeSFF = new System.Windows.Forms.ToolStripMenuItem();
+            this.SaveSFFFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.SaveSFFDialog = new System.Windows.Forms.SaveFileDialog();
+            this.MainTab.SuspendLayout();
+            this.UserRythm.SuspendLayout();
+            this.UserRythmMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(371, 18);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(46, 25);
+            this.OpenButton.TabIndex = 0;
+            this.OpenButton.Text = "開く";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // OpenRegistDialog
+            // 
+            this.OpenRegistDialog.Filter = "ELレジストファイル (*.B00)|*.B00";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(343, 19);
+            this.textBox1.TabIndex = 2;
+            // 
+            // MainTab
+            // 
+            this.MainTab.Controls.Add(this.UserRythm);
+            this.MainTab.Location = new System.Drawing.Point(17, 55);
+            this.MainTab.Name = "MainTab";
+            this.MainTab.SelectedIndex = 0;
+            this.MainTab.Size = new System.Drawing.Size(400, 381);
+            this.MainTab.TabIndex = 3;
+            // 
+            // UserRythm
+            // 
+            this.UserRythm.Controls.Add(this.SaveAllSFF);
+            this.UserRythm.Controls.Add(this.SaveSelectedSFF);
+            this.UserRythm.Controls.Add(this.UserRythmList);
+            this.UserRythm.Location = new System.Drawing.Point(4, 22);
+            this.UserRythm.Name = "UserRythm";
+            this.UserRythm.Padding = new System.Windows.Forms.Padding(3);
+            this.UserRythm.Size = new System.Drawing.Size(392, 355);
+            this.UserRythm.TabIndex = 0;
+            this.UserRythm.Text = "ユーザーリズム";
+            this.UserRythm.UseVisualStyleBackColor = true;
+            // 
+            // SaveAllSFF
+            // 
+            this.SaveAllSFF.Enabled = false;
+            this.SaveAllSFF.Location = new System.Drawing.Point(204, 318);
+            this.SaveAllSFF.Name = "SaveAllSFF";
+            this.SaveAllSFF.Size = new System.Drawing.Size(174, 22);
+            this.SaveAllSFF.TabIndex = 2;
+            this.SaveAllSFF.Text = "空でないすべてのリズムを保存";
+            this.SaveAllSFF.UseVisualStyleBackColor = true;
+            this.SaveAllSFF.Click += new System.EventHandler(this.SaveAllSFF_Click);
+            // 
+            // SaveSelectedSFF
+            // 
+            this.SaveSelectedSFF.Enabled = false;
+            this.SaveSelectedSFF.Location = new System.Drawing.Point(13, 318);
+            this.SaveSelectedSFF.Name = "SaveSelectedSFF";
+            this.SaveSelectedSFF.Size = new System.Drawing.Size(174, 23);
+            this.SaveSelectedSFF.TabIndex = 1;
+            this.SaveSelectedSFF.Text = "選択されたリズムを保存";
+            this.SaveSelectedSFF.UseVisualStyleBackColor = true;
+            this.SaveSelectedSFF.Click += new System.EventHandler(this.SaveSelectedSFF_Click);
+            // 
+            // UserRythmList
+            // 
+            this.UserRythmList.AutoArrange = false;
+            this.UserRythmList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.UserNumber,
+            this.SFFStatus,
+            this.SFFSize});
+            this.UserRythmList.ContextMenuStrip = this.UserRythmMenu;
+            this.UserRythmList.FullRowSelect = true;
+            this.UserRythmList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.UserRythmList.HideSelection = false;
+            this.UserRythmList.LabelWrap = false;
+            this.UserRythmList.Location = new System.Drawing.Point(13, 17);
+            this.UserRythmList.Name = "UserRythmList";
+            this.UserRythmList.ShowGroups = false;
+            this.UserRythmList.Size = new System.Drawing.Size(365, 287);
+            this.UserRythmList.TabIndex = 0;
+            this.UserRythmList.UseCompatibleStateImageBehavior = false;
+            this.UserRythmList.View = System.Windows.Forms.View.Details;
+            // 
+            // UserNumber
+            // 
+            this.UserNumber.Text = "ユーザーNo.";
+            this.UserNumber.Width = 70;
+            // 
+            // SFFStatus
+            // 
+            this.SFFStatus.Text = "状態";
+            this.SFFStatus.Width = 210;
+            // 
+            // SFFSize
+            // 
+            this.SFFSize.Text = "サイズ";
+            this.SFFSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // UserRythmMenu
+            // 
+            this.UserRythmMenu.AllowMerge = false;
+            this.UserRythmMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.RenameAndSaveSFF,
+            this.ReadSFF,
+            this.InitializeSFF});
+            this.UserRythmMenu.Name = "UserRythmMenu";
+            this.UserRythmMenu.ShowImageMargin = false;
+            this.UserRythmMenu.Size = new System.Drawing.Size(138, 70);
+            // 
+            // RenameAndSaveSFF
+            // 
+            this.RenameAndSaveSFF.Enabled = false;
+            this.RenameAndSaveSFF.Name = "RenameAndSaveSFF";
+            this.RenameAndSaveSFF.Size = new System.Drawing.Size(137, 22);
+            this.RenameAndSaveSFF.Text = "名前を付けて保存";
+            this.RenameAndSaveSFF.Click += new System.EventHandler(this.RenameAndSaveSFF_Click);
+            // 
+            // ReadSFF
+            // 
+            this.ReadSFF.Enabled = false;
+            this.ReadSFF.Name = "ReadSFF";
+            this.ReadSFF.Size = new System.Drawing.Size(137, 22);
+            this.ReadSFF.Text = "読み込み";
+            // 
+            // InitializeSFF
+            // 
+            this.InitializeSFF.Enabled = false;
+            this.InitializeSFF.Name = "InitializeSFF";
+            this.InitializeSFF.Size = new System.Drawing.Size(137, 22);
+            this.InitializeSFF.Text = "削除";
+            // 
+            // SaveSFFFolderDialog
+            // 
+            this.SaveSFFFolderDialog.Description = "出力先フォルダーを選択してください。";
+            // 
+            // SaveSFFDialog
+            // 
+            this.SaveSFFDialog.DefaultExt = "*.sty";
+            this.SaveSFFDialog.Filter = "スタイルファイル|*.sty|すべてのファイル|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 451);
+            this.Controls.Add(this.MainTab);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.OpenButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "EL Data Analyzer v1.00";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MainTab.ResumeLayout(false);
+            this.UserRythm.ResumeLayout(false);
+            this.UserRythmMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.OpenFileDialog OpenRegistDialog;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl MainTab;
+        private System.Windows.Forms.TabPage UserRythm;
+        private System.Windows.Forms.ListView UserRythmList;
+        private System.Windows.Forms.ColumnHeader UserNumber;
+        private System.Windows.Forms.ColumnHeader SFFStatus;
+        private System.Windows.Forms.ColumnHeader SFFSize;
+        private System.Windows.Forms.Button SaveAllSFF;
+        private System.Windows.Forms.Button SaveSelectedSFF;
+        private System.Windows.Forms.FolderBrowserDialog SaveSFFFolderDialog;
+        private System.Windows.Forms.SaveFileDialog SaveSFFDialog;
+        private System.Windows.Forms.ContextMenuStrip UserRythmMenu;
+        private System.Windows.Forms.ToolStripMenuItem RenameAndSaveSFF;
+        private System.Windows.Forms.ToolStripMenuItem ReadSFF;
+        private System.Windows.Forms.ToolStripMenuItem InitializeSFF;
+    }
+}
+
---
+++ elanalyser/Form1.cs
@@ -0,0 +1,326 @@
+﻿/*                                        */
+/*         EL Data Analyzer v1.00         */
+/*              (2015/10/05)              */
+/*                                        */
+/*  Coded by 13期の駆け出しプログラマー   */
+/*                                        */
+
+
+/* 初期定義 */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/* 名前空間 */
+
+namespace elanalyser
+{
+
+    /* Form1 */
+
+    public partial class Form1 : Form
+    {
+
+        /* リストの生成 */
+
+        List<byte[]> SFFList = new List<byte[]>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        /* Form1のロード完了 */
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        /* 開くボタンを押したときの動作 */
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            /* ファイルを開くダイアログからパスを取得 */
+            if (OpenRegistDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (FileRead(OpenRegistDialog.FileName) == true)
+                {
+                    /* ユーザーリズムタブの表示 */
+                    UserRythm_Show();
+
+                    /* 操作ボタンの有効化 */
+                    SaveSelectedSFF.Enabled = true;
+                    SaveAllSFF.Enabled = true;
+                    RenameAndSaveSFF.Enabled = true;
+                }
+            }
+        }
+
+
+
+        /* 選択保存ボタンを押したときの動作 */
+
+        private void SaveSelectedSFF_Click(object sender, EventArgs e)
+        {
+            /* 1つ以上のリズムユーザーが選択されていることを確認 */
+            if (UserRythmList.SelectedIndices.Count > 0)
+            {
+                /* フォルダ選択ダイアログを開き保存先パスを取得 */
+                try
+                {
+                    if (SaveSFFFolderDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        /* 変数宣言 */
+                        int i = new int();
+                        int k = new int();
+
+                        /* 選択されたSFF */
+                        for (i = 0; i < UserRythmList.SelectedIndices.Count; i++)
+                        {
+                            k = UserRythmList.SelectedIndices[i];
+
+                            /* ファイルストリームを用いて保存 */
+                            using (FileStream fs = new FileStream(SaveSFFFolderDialog.SelectedPath + @"/User" + (k+1).ToString() + ".sty", FileMode.Create, FileAccess.Write))
+                            {
+                                fs.Write(SFFList[k], 0, SFFList[k].Length);
+                            }
+                        }
+
+                        /* 完了報告 */
+                        MessageBox.Show(UserRythmList.SelectedIndices.Count.ToString() + "個のStyleファイルを出力しました。", "完了", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("ファイルの保存に失敗しました。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            /* ユーザー未選択エラー */
+            else
+            {
+                MessageBox.Show("リズムユーザーが選択されていません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        /* すべて保存ボタンを押したときの動作 */
+
+        private void SaveAllSFF_Click(object sender, EventArgs e)
+        {
+            /* フォルダ選択ダイアログを開き保存先パスを取得 */
+            try
+            {
+                if (SaveSFFFolderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    /* 変数宣言 */
+                    int i = new int();
+                    int k = new int();
+                    k = 0;
+
+                    /* すべてのSFF */
+                    for (i = 0; i < 48; i++)
+                    {
+                        /* ファイルストリームを用いて保存 */ 
+                        if (SFFList[i].Length > 388)
+                        {
+                            using (FileStream fs = new FileStream(SaveSFFFolderDialog.SelectedPath + @"/User" + (i + 1).ToString() + ".sty", FileMode.Create, FileAccess.Write))
+                            {
+                                fs.Write(SFFList[i], 0, SFFList[i].Length);
+                            }
+
+                            k++;
+                        }
+                    }
+
+                    /* 完了報告 */
+                    MessageBox.Show(k.ToString() + "個のStyleファイルを出力しました。", "完了", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+            /* 保存エラー */
+            catch
+            {
+                MessageBox.Show("ファイルの保存に失敗しました。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        /* 名前を付けて保存ボタンを押したときの動作 */
+
+        private void RenameAndSaveSFF_Click(object sender, EventArgs e)
+        {
+            /* 変数宣言 */
+            int i = new int();
+
+            /* 選択されているすべてのユーザーについて処理を行う */
+            for (i = 0; i < UserRythmList.SelectedIndices.Count; i++)
+            {
+                /* デフォルトの保存名を指定 */
+                SaveSFFDialog.FileName = "User" + (UserRythmList.SelectedIndices[i]+1).ToString() + ".sty";
+
+                try
+                {
+                    /* FileStreamを用いて保存 */
+                    if (SaveSFFDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        using (FileStream fs = new FileStream(SaveSFFDialog.FileName, FileMode.Create, FileAccess.Write))
+                        {
+                            fs.Write(SFFList[i], 0, SFFList[i].Length);
+                        }
+                    }
+                }
+
+                /* 保存エラー */
+                catch
+                {
+                    MessageBox.Show("ファイルの保存に失敗しました。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+
+        /* ファイルの読み込み */
+
+        private bool FileRead(string FilePass)
+        {
+            /* 変数定義 */
+            byte[] REGIST = new byte[1];
+
+            /* ファイルの内容をREGISTに読み込み */
+            using (FileStream fs = new FileStream(FilePass, FileMode.Open, FileAccess.Read))
+            {
+                try
+                {
+                    Array.Resize(ref REGIST, (int)fs.Length);
+                    fs.Read(REGIST, 0, REGIST.Length);
+                }
+
+                /* ファイルが開けなかったときは異常終了 */
+                catch
+                {
+                    MessageBox.Show("ファイルが開けません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+
+            }
+
+            /* ファイルヘッダを確認 */
+            if (REGIST[0] == 240 && REGIST[1] == 67 && REGIST[2] == 112 && REGIST[3] == 120 && REGIST[4] == 0 && REGIST[5] == 60 && REGIST[6] == 0 && REGIST[7] == 127)
+            {
+
+                /* ローカル変数定義 */
+                int[] SFF_head = new int[49];
+                byte[] SFF_temp = new byte[1];
+                byte head = 0;
+
+                /* リストのクリア */
+                SFFList.Clear();
+
+
+                /* ユーザーリズムの読み込み */
+
+                /* ヘッダの検出 */
+                for (int n = 0; n < REGIST.Length - 7; n++)
+                {
+                    if (REGIST[n] == 77 && REGIST[n + 1] == 84 && REGIST[n + 2] == 104 && REGIST[n + 3] == 100 && REGIST[n + 4] == 0 && REGIST[n + 5] == 0 && REGIST[n + 6] == 0 && REGIST[n + 7] == 6)
+                    {
+                        SFF_head[head] = n;
+                        head++;
+                    }
+                }
+
+                /* ヘッダをもとに分割 */
+                if (head == 48)
+                {
+                    SFF_head[48] = REGIST.Length - 2;
+
+                    for (byte k = 0; k < 48; k++)
+                    {
+                        Array.Resize(ref SFF_temp, SFF_head[k + 1] - SFF_head[k]);
+
+                        for (int i = 0; i < SFF_head[k + 1] - SFF_head[k]; i++)
+                        {
+                            SFF_temp[i] = REGIST[SFF_head[k] + i];
+                        }
+
+                        SFFList.Add(SFF_temp);
+                    }
+
+                }
+
+                /* ヘッダが48個検出されなかったときは異常終了 */
+                else
+                {
+                    MessageBox.Show("データの読み込みに失敗しました。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+
+                /* ファイルパスを表示 */
+                textBox1.Text = FilePass;
+
+                return true;
+
+            }
+
+            /* ファイルヘッダ検出ができなかったときは異常終了 */
+            else
+            {
+                MessageBox.Show("ELデータファイルではありません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+
+
+        /* ユーザーリズムタブの表示 */
+
+        private void UserRythm_Show()
+        {
+            /* ユーザーリズムリスト初期化 */
+            UserRythmList.Items.Clear();
+
+            /* ユーザーリズムリスト表示 */
+            for (byte k = 0; k < 48; k++)
+            {
+                ListViewItem UR = new ListViewItem("User" + (k + 1).ToString());
+
+                if (SFFList[k].Length > 388)
+                {
+                    UR.SubItems.Add("Original");
+                    UR.ForeColor = Color.Black;
+                }
+
+                else
+                {
+                    UR.SubItems.Add("Empty");
+                    UR.ForeColor = Color.Gray;
+                }
+
+                UR.SubItems.Add(SFFList[k].Length.ToString());
+
+                UserRythmList.Items.Add(UR);
+            }
+        }
+
+    }
+}
